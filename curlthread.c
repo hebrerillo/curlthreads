305c97a08c556632c11c668580227a8585582e98
Jesus <hebrerillo@gmail.com>
2016-01-10 16:58:32 +0100

    First version

--- curlthread.c
+++ curlthread.c
@@ -1,1 +1,78 @@
+#include "curlthread.h"
+
+int performCurlThreads(List *l)
+{
+    int i;
+    int initialListSize;
+    if (l == NULL || l->head == NULL || l->size == 0)//check empty list
+    {
+        return 0;
+    }
+
+    pthread_t *threads = (pthread_t*) malloc(sizeof (pthread_t) * l->size);
+    ListElement *current;
+
+    initialListSize = l->size; //saving the list size, as pop(l) will modify the list size
+    i = 0;
+    while ((current = pop(l)) != NULL && i < initialListSize)
+    {
+        //for each element of the list, create a thread to handle the curl request
+        pthread_create(&threads[i], NULL, curl_thread_func, current->data);
+        i++;
+    }
+
+    i = 0;
+    while (i < initialListSize)
+    {
+        pthread_join(threads[i], NULL);
+        i++;
+    }
+    free(threads);
+    return 1;
+}
+
+void *curl_thread_func(void *ptr)
+{
+    curlRequest *s = (curlRequest*) ptr;
+
+    CURL* curl;
+    CURLcode res;
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, s->url);
+    curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &curlCallback);
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, s);
+
+    res = curl_easy_perform(curl);
+    if (res != CURLE_OK)
+    {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        s->error = curl_easy_strerror(res);
+        s->resCode = res;
+    }
+    curl_easy_cleanup(curl);
+
+    return NULL;
+}
+
+
+size_t curlCallback(char* ptr, size_t size, size_t nmemb, curlRequest *s)
+{
+    size_t ssize = nmemb * size;
+
+    s->result = (char*)malloc(ssize + 1);
+
+    if (s->result == NULL)
+    {
+        fprintf(stderr, "malloc() failed to allocate memory to s->result\n");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(s->result, ptr, ssize);
+    s->result[ssize] = '\0';
+    
+    return ssize;
+}
 
--- curlthread.h
+++ curlthread.h
@@ -3,6 +3,8 @@
 #include <curl/curl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include "list.h"
 
 #ifdef	__cplusplus
 extern "C"
@@ -15,15 +17,46 @@ extern "C"
    */
   typedef struct
   {
-    char *url;
-    char *type; //Type of the request: GET or POST
-    char *params; //Request parameters (only if type is POST)
+    const char *url;
+    const char *type; //Type of the request: GET or POST
+    const char *params; //Request parameters (only if type is POST)
     char *result; //The result returned by the curl request
-    CURLcode res; //The result code of the curl request
-    char *error; //Error string just in case the request fails
+    CURLcode resCode; //The result code of the curl request
+    const char *error; //Error string just in case the request fails
   } curlRequest;
 
   
+  
+  
+  /**
+   * Performs several curl calls, each one running in a separate thread. 
+   * The list l contains curlRequest structs, each one defining the curl parameters for each call
+   * 
+   * @param l A list containing curlRequest structs
+   */
+  int performCurlThreads(List *l);
+  
+  
+  
+  /**
+   * 
+   * Handles the result of a curl request. It will save the result to s->result
+   * @param ptr
+   * @param size
+   * @param nmemb
+   * @param s A pointer to curlRequest struct
+   * @return The number of bytes handled by the curl request
+   */
+  size_t curlCallback(char* ptr, size_t size, size_t nmemb, curlRequest *s);
+  
+  
+  
+  /**
+   * Handles the execution of a thread
+   * @param ptr
+   * @return 
+   */
+  void *curl_thread_func(void *ptr);
 
 #ifdef	__cplusplus
 }
