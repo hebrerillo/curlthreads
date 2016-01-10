87feaeda82e4c9300357bcb83ed49bc6cc770f7e
Jesus <hebrerillo@gmail.com>
2016-01-10 10:35:58 +0100

    struct curlRequest to save all the information related to a curl request

--- curlthread.h
+++ curlthread.h
@@ -1,13 +1,27 @@
 #ifndef CURLTHREAD_H
 #define	CURLTHREAD_H
+#include <curl/curl.h>
+#include <stdlib.h>
+#include <string.h>
 
 #ifdef	__cplusplus
 extern "C"
 {
 #endif
 
+  /**
+   * This struct saves all the information required
+   * to perform a curl request
+   */
+  typedef struct
+  {
+    char *url;
+    char *type; //Type of the request: GET or POST
+    char *params; //Request parameters (only if type is POST)
+    char *result; //The result returned by the curl request
+  } curlRequest;
 
-
+  
 
 #ifdef	__cplusplus
 }
